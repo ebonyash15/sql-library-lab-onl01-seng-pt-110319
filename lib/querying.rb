3f15aa776167ed1c63bff7ad05a586a64ca6dcb6
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-26 15:53:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year
+  FROM books
+  INNER JOIN series
+  ON Books.series_id = Series.id
+  WHERE Series.id = 1
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT Characters.name, Characters.motto"
 end
 
 

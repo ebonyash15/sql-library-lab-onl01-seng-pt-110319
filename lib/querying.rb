856ab735d2e78ddf54cec787366de4310b33e9f6
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-26 16:53:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,5 +34,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT names, COUNT(book_id) FROM characters GROUP BY book_id"
+  "SELECT name, COUNT(book_id) FROM characters GROUP BY book_id"
 end

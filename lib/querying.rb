3ea7e1e18ff53a722dd24f0082464368a6b4c7d9
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-26 16:19:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT books.title FROM(
+    SELECT Series.title AS title, COUNT(characters.series_id) AS species_number
+    FROM series
+    INNER JOiN characters
+    ON Series.id = Characters.series_id
+    WHERE Characters.species = 'human'
+    GROUP BY Characters.series_id
+    ORDER BY species_number DESC LIMIT 1)"
 end
 
 def select_character_names_and_number_of_books_they_are_in

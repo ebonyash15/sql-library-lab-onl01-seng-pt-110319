14d12345f19155a715d167ecb6ac960179dc6b67
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-26 16:50:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN series ON Series.author_id = Authors.id
   WHERE Characters.species = 'human'
   GROUP BY Series.author_id
-  ORDER BY COUNT(Characters.species)"
+  ORDER BY COUNT(Characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
